111127578c02a4b75a404ddfdd8bf52444efdf44
SevenYom <88223274+SevenYom@users.noreply.github.com>
2023-06-05 15:47:41 +0800

    Initial commit

---
+++ components/AppDemo.vue
@@ -0,0 +1,3 @@
+<template>
+  <span> demo </span>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Welcome to the pages11111111111</h1>
+    <AppAlert> This is an auto-imported component </AppAlert>
+  </div>
+</template>
+  
