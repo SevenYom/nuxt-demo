5e57179c95575e8d54e32003ae9cfc020ef7d51b
SevenYom <88223274+SevenYom@users.noreply.github.com>
2023-06-05 15:52:55 +0800

    1 file modified

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <!-- <NuxtWelcome /> -->
+    <button>123</button>
     <h1>Welcome to the homepage</h1>
     <AppAlert> This is an auto-imported component. </AppAlert>
     <br />
     <AppDemo></AppDemo>
   </div>
 </template>
+<script></script>
